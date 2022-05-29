d88cc3eb1060c9c2eff51aea4b027e7932fffb15
sughimura <nxsughi@gmail.com>
2022-05-29 17:41:39 +0900

    ac: update ITP2_1_B_Deque.go
    ref: https://onlinejudge.u-aizu.ac.jp/solutions/problem/ITP2_1_B/review/3254827/chroju/Go

--- aoj/ITP2/ITP2_1_B_Deque.go
+++ aoj/ITP2/ITP2_1_B_Deque.go
@@ -8,10 +8,32 @@ import (
 	"strings"
 )
 
+func push(queue []int, pushPosition int, value int, head int, tail int) ([]int, int, int) {
+	if pushPosition == 0 {
+		head--
+		queue[head] = value
+	} else {
+		queue[tail] = value
+		tail++
+	}
+	return queue, head, tail
+}
+
+func pop(popPosition int, head int, tail int) (int, int) {
+	if popPosition == 0 {
+		head++
+	} else {
+		tail--
+	}
+	return head, tail
+}
+
 func main() {
 	var q int
 	fmt.Scan(&q)
-	var A []int
+	A := make([]int, q*2)
+	head := q
+	tail := head
 	scanner := bufio.NewScanner(os.Stdin)
 	for i := 0; i < q; i++ {
 		scanner.Scan()
@@ -19,21 +41,13 @@ func main() {
 		if query[0] == "0" {
 			d, _ := strconv.Atoi(query[1])
 			x, _ := strconv.Atoi(query[2])
-			if d == 0 {
-				A = append([]int{x}, A...)
-			} else {
-				A = append(A, x)
-			}
+			A, head, tail = push(A, d, x, head, tail)
 		} else if query[0] == "1" {
 			p, _ := strconv.Atoi(query[1])
-			fmt.Println(A[p])
+			fmt.Println(A[head+p])
 		} else if query[0] == "2" {
 			d, _ := strconv.Atoi(query[1])
-			if d == 0 {
-				A = A[1:]
-			} else {
-				A = A[:len(A)-1]
-			}
+			head, tail = pop(d, head, tail)
 		}
 	}
 }
