3ca98f03152845fdaf449b33ca17d7349e3c0904
sughimura <nxsughi@gmail.com>
2022-06-04 18:50:40 +0900

    feat: add ALDS1_1_A_InsertionSort3.go

---
+++ aoj/ALDS1/ALDS1_1_A_InsertionSort3.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	A := make([]int, n)
+	printA := func() {
+		for i := 0; i < n; i++ {
+			fmt.Print(A[i])
+			if i == n-1 {
+				fmt.Print("\n")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+	}
+	for i := 0; i < n; i++ {
+		fmt.Scan(&A[i])
+	}
+	for i := 1; i < n; i++ {
+		printA()
+		v := A[i]
+		j := i - 1
+		for j >= 0 && A[j] > v {
+			A[j+1] = A[j]
+			j--
+		}
+		A[j+1] = v
+	}
+	printA()
+}
